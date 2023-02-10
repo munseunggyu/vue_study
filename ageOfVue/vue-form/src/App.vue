87f186c2ad7f79e446bf5e3445931bcd39e07bc9
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-02-10 22:48:25 +0900

    vue-form 예제

---
+++ ageOfVue/vue-form/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <form v-on:submit.prevent="handleLogin">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleLogin: function () {
+      // e.preventDefault();
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
