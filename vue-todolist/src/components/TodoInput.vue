a0ad4f71f99160b9f0add7966b94913b9073fdd7
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-02-11 12:20:24 +0900

    todoinput 스타일링

--- vue-todolist/src/components/TodoInput.vue
+++ vue-todolist/src/components/TodoInput.vue
@@ -1,9 +1,58 @@
 <template>
-  <div>input</div>
+  <form class="inputBox shadow" v-on:submit.prevent="addTodo">
+    <input type="text" v-model="task" />
+    <button class="addBtn">+</button>
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      task: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      console.log(this.task);
+      localStorage.setItem(
+        "todo",
+        JSON.stringify({
+          task: this.task,
+          id: new Date(),
+        })
+      );
+      this.clearInput();
+    },
+    clearInput: function () {
+      this.task = "";
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addBtn {
+  border: 0;
+  border-radius: 0 5px 5px 0;
+  width: 3rem;
+  height: 100%;
+  float: right;
+  font-size: 2rem;
+  color: white;
+  background: #6478fb;
+}
+</style>
