97d9f380e11d900c462b2c74a922ce36819404c6
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-02-18 20:34:55 +0900

    vue-todo-api

---
+++ vue-todo-api/README.md
@@ -0,0 +1,9 @@
+# API 실행
+
+1. back
+
+- npm i && npm run start
+
+2. front
+
+- npm i && npm run serve
---
+++ vue-todo-api/front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>hi</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created() {
+    console.log(process.env.VUE_APP_API);
+    axios
+      .post("http://localhost:5500/users/login", {
+        email: "e1@naver.com",
+        password: "123123aa",
+      })
+      .then((res) => console.log(res));
+  },
+};
+</script>
+
+<style></style>
