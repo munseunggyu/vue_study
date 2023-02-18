e5bbe8e8b6faf3ace0107900bb1e535208d5acfa
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-02-18 15:14:27 +0900

    vue-chart bar,line chart 예제

---
+++ vue-chart/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Chart</h1>
+    <BarChart></BarChart>
+    <LineChart></LineChart>
+  </div>
+</template>
+
+<script>
+import BarChart from "./components/BarChart.vue";
+import LineChart from "./components/LineChart.vue";
+export default {
+  components: {
+    BarChart,
+    LineChart,
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-chart/src/components/LineChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="width: 800px"><canvas id="line"></canvas></div>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  mounted() {
+    (async function () {
+      const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+      ];
+
+      new Chart(document.getElementById("line"), {
+        type: "line",
+        data: {
+          labels: data.map((row) => row.year),
+          datasets: [
+            {
+              label: "Acquisitions by year",
+              data: data.map((row) => row.count),
+            },
+          ],
+        },
+      });
+    })();
+  },
+};
+</script>
+
+<style></style>
