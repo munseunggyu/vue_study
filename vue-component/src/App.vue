8894e1808b1518eb892d9f500ae1f1e7bb0d620f
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-02-18 16:57:06 +0900

    vue-component Renderless Component 예제

--- vue-component/src/App.vue
+++ vue-component/src/App.vue
@@ -3,28 +3,43 @@
     <h1>Controlled Component</h1>
     <section>
       <h2>CheckBox</h2>
-      <CheckBoxInput v-model="hi"></CheckBoxInput>
+      <CheckBoxInput v-model="checkInput"></CheckBoxInput>
     </section>
     <section>
       <h2>SelectBox</h2>
-      <SelectBox v-model="hey"></SelectBox>
+      <SelectBox v-model="selectInput"></SelectBox>
+    </section>
+    <hr />
+    <section>
+      <h2>Renderless Component</h2>
+      <FetchData :url="url">
+        <div slot-scope="{ response, loading }">
+          <div v-if="!loading">
+            {{ response }}
+          </div>
+          <div v-if="loading">...loading</div>
+        </div>
+      </FetchData>
     </section>
   </div>
 </template>
-
+<!-- https://jsonplaceholder.typicode.com/users/1 -->
 <script>
 import CheckBoxInput from "./controlledComponent/CheckBoxInput.vue";
 import SelectBox from "./controlledComponent/SelectBox.vue";
+import FetchData from "./renderlessComponent/FetchData.vue";
 
 export default {
   components: {
     CheckBoxInput,
     SelectBox,
+    FetchData,
   },
   data() {
     return {
-      hi: false,
-      hey: "all",
+      checkInput: false,
+      selectInput: "all",
+      url: "https://jsonplaceholder.typicode.com/users/1",
     };
   },
   methods: {},
