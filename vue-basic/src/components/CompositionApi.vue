f1491fea20ae58b5f9546a30a3335101e4754ece
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-02-25 01:09:16 +0900

    vue-basic composition api ex

---
+++ vue-basic/src/components/CompositionApi.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>Reactive Message</h2>
+    <p>
+      {{ reactiveMessage }}
+    </p>
+    <button @click="addHi">click</button>
+    <h2>Nomal Message</h2>
+    <p>
+      {{ nomalMessage }}
+    </p>
+    <button @click="addHey">click</button>
+  </div>
+</template>
+
+<script>
+import { ref, isRef, onMounted, onBeforeMount } from "vue";
+export default {
+  setup() {
+    console.log("setup");
+    const reactiveMessage = ref("Reactive Message");
+    const addHi = () => {
+      reactiveMessage.value = reactiveMessage.value + " hi";
+    };
+    console.log("isRef(reactiveMessage): ", isRef(reactiveMessage));
+
+    let nomalMessage = "Nomal Message";
+    const addHey = () => {
+      nomalMessage = nomalMessage + " hey";
+    };
+    console.log("isRef(nomalMessage): ", isRef(nomalMessage));
+
+    onMounted(() => {
+      console.log("onMounted");
+    });
+
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+
+    return {
+      reactiveMessage,
+      addHi,
+      addHey,
+      nomalMessage,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-basic/src/components/CounterEx.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="increment">click</button>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const counter = ref(0);
+    const increment = () => counter.value++;
+
+    return {
+      counter,
+      increment,
+    };
+  },
+};
+</script>
+
+<style></style>
